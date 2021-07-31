2cb46a9ceada85c4bd7fc5bfd526a23f35e246c6
Valera <example@gmail.com>
2021-07-31 18:22:48 +0300

    MovieDB creation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 "use strict"
 
+const numberOfFilms = +prompt('Сколько фильмов, вы посмотрели?', '');
+
+
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('Оценка', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('Оценка', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
